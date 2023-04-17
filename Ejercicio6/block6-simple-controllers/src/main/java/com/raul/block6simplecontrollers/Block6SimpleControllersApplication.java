a181c9a0d202d52823ff292002255e442f6999d6
RaulRodriguez71 <rodriguezmontavezraul71@gmail.com>
2023-04-17 10:23:54 +0200

    Apartado 1 Ejercicio 6

---
+++ Ejercicio6/block6-simple-controllers/src/main/java/com/raul/block6simplecontrollers/Block6SimpleControllersApplication.java
@@ -0,0 +1,16 @@
+package com.raul.block6simplecontrollers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Block6SimpleControllersApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Block6SimpleControllersApplication.class, args);
+	}
+
+
+
+}
---
+++ Ejercicio6/block6-simple-controllers/src/main/java/com/raul/block6simplecontrollers/Persona.java
@@ -0,0 +1,47 @@
+package com.raul.block6simplecontrollers;
+
+public class Persona {
+    private String nombre;
+    private String poblacion;
+    private int edad;
+
+    public Persona(String nombre, String poblacion, int edad) {
+        this.nombre = nombre;
+        this.poblacion = poblacion;
+        this.edad = edad;
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPoblacion() {
+        return poblacion;
+    }
+
+    public void setPoblacion(String poblacion) {
+        this.poblacion = poblacion;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "nombre='" + nombre + '\'' +
+                ", poblacion='" + poblacion + '\'' +
+                ", edad=" + edad +
+                '}';
+    }
+}
