458cfc1414f2df54e81248ac4b0677b4b46456a4
RaulRodriguez71 <rodriguezmontavezraul71@gmail.com>
2023-04-24 14:12:26 +0200

    Ejercicio 1 bloque 7 Apartado de eliminar

--- Ejercicio7/block7-crud/src/main/java/com/raul/block7crud/controllers/BorrarPersona.java
+++ Ejercicio7/block7-crud/src/main/java/com/raul/block7crud/controllers/BorrarPersona.java
@@ -3,18 +3,15 @@ package com.raul.block7crud.controllers;
 import com.raul.block7crud.Application.PersonaServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/persona")
 public class BorrarPersona {
     @Autowired
     PersonaServiceImpl personaServiceimpl ;
-    @DeleteMapping
-    public ResponseEntity<String> deletePersonaById(@RequestParam int id) {
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deletePersonaById(@PathVariable int id) {
         try {
             personaServiceimpl.deletePersonaById(id);
             return ResponseEntity.ok().body("persona with id: "+id+" was deleted");
