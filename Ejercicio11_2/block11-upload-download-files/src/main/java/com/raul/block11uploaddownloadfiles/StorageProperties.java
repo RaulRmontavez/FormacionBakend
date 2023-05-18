b29fd479075dd900f6b5a3448d3c362086256e35
RaulRodriguez71 <rodriguezmontavezraul71@gmail.com>
2023-05-18 14:58:55 +0200

    Bloque 11 Ejercicio 2 Comienzo

---
+++ Ejercicio11_2/block11-upload-download-files/src/main/java/com/raul/block11uploaddownloadfiles/StorageProperties.java
@@ -0,0 +1,40 @@
+package com.raul.block11uploaddownloadfiles;
+
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Path;
+import java.util.stream.Stream;
+@Service
+public class StorageProperties implements StorageService {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void store(MultipartFile file) {
+
+    }
+
+    @Override
+    public Stream<Path> loadAll() {
+        return null;
+    }
+
+    @Override
+    public Path load(String filename) {
+        return null;
+    }
+
+    @Override
+    public Resource loadAsResource(String filename) {
+        return null;
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+}
